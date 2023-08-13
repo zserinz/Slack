04e3755f4013ea21c2e2edc90e19b7899c7e621a
zserinz <141047914+zserinz@users.noreply.github.com>
2023-08-14 00:05:33 +0900

    Delete files

--- ToDo-Front-Page/src/calculator.js
+++
@@ -1,6 +0,0 @@
-const onOff = document.querySelector(".onoff");
-const value = document.querySelector(".value");
-
-onOff.addEventListener("click", function () {
-    value.classList.toggle("num-hidden");
-});
