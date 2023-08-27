5bba489b6b2f703ca0d26c58cda29e1c2dd68417
zserinz <serinn1023@gmail.com>
2023-08-27 21:09:16 +0900

    feat: 회원가입 api 연결

--- sleact-master/alecture/pages/SignUp/index.tsx
+++ sleact-master/alecture/pages/SignUp/index.tsx
@@ -13,6 +13,8 @@ const SignUp = () => {
   const [password, ,setPassword] = useInput('');
   const [passwordCheck, ,setPasswordCheck] = useInput('');
   const [mismatchError, setMismatchError] = useState(false);
+  const [signUpError, setSignUpError] = useState('')
+  const [signUpSuccess, setSignUpSuccess] = useState(false)
 
   const onChangePassword = useCallback((e: any) => {
     setPassword(e.target.value);
@@ -27,7 +29,21 @@ const SignUp = () => {
   const onSubmit = useCallback((e: any)=> {
     e.preventDefault();
     if (!mismatchError) {
-      console.log('서버로 회원가입')
+      // state initialize(loading)
+      setSignUpError('')
+      setSignUpSuccess(false)
+
+      // api
+      axios.post('/api/users', {
+        email, nickname, password
+      })
+        .then(()=>{
+          setSignUpSuccess(true);
+        })
+        .catch((error)=>{
+          setSignUpError(error.response.data)
+        })
+        .finally(()=>{});
     }
   },[email, nickname, password, passwordCheck])
 
@@ -65,9 +81,9 @@ const SignUp = () => {
                 />
               </div>
               {mismatchError && <Error>비밀번호가 일치하지 않습니다.</Error>}
-              {/* {!nickname && <Error>닉네임을 입력해주세요.</Error>}
+              {!nickname && <Error>닉네임을 입력해주세요.</Error>}
               {signUpError && <Error>{signUpError}</Error>}
-              {signUpSuccess && <Success>회원가입되었습니다! 로그인해주세요.</Success>} */}
+              {signUpSuccess && <Success>회원가입되었습니다! 로그인해주세요.</Success>}
             </Label>
             <Button type="submit">회원가입</Button>
           </Form>
